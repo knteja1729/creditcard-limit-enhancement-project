e580b0672ee2d146ec72ad0b637ac1245b090c33
narendrateja123 <narendrateja@aitacs.com>
2024-04-01 23:32:47 +0530

    first commit

---
+++ src/main/java/com/tesco/enquiry/service/CreditLmitEnquiryServiceImpl.java
@@ -0,0 +1,12 @@
+/* @Copyright 2023, All Rights are reserved, Tesco Bank pvt ltd. we should not disclose the information outside
+ * otherwise terms and conditions will apply
+ */
+package com.tesco.enquiry.service;
+
+/**
+ * @author NarendraTeja by 06-Aug-2023
+ *
+ */
+public class CreditLmitEnquiryServiceImpl {
+
+}
